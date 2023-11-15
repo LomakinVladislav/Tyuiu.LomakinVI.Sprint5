7b58db21617ce0f9b6aeec966ed7f5a952dc9f8e
Tyuiu.LomskinVI@outlook.com <winr2005@gmail.com>
2023-11-15 19:24:06 +0500

    Task1

--- Tyuiu.LomakinVI.Sprint5.Task1.V29.Test/DataServiceTest.cs
+++ Tyuiu.LomakinVI.Sprint5.Task1.V29.Test/DataServiceTest.cs
@@ -1,17 +1,17 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using Tyuiu.LomakinVI.Sprint5.Task0.V25.Lib;
 using System.IO;
+using Tyuiu.LomakinVI.Sprint5.Task1.V29.Lib;
 
-namespace Tyuiu.LomakinVI.Sprint5.Task0.V25.Test
+namespace Tyuiu.LomakinVI.Sprint5.Task1.V29.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
         public void TestMethod1()
-        {
-            string path = @"C:\Users\OneSmiLe\source\repos\Tyuiu.LomakinVI.Sprint5\Tyuiu.LomakinVI.Sprint5.Task0.V25\bin\Debug\OutPutFileTask0.txt";
+        {   
+            string path = @"C:\Users\OneSmiLe\source\repos\Tyuiu.LomakinVI.Sprint5\Tyuiu.LomakinVI.Sprint5.Task1.V29\bin\Debug\OutPutFileTask1.txt";
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
---
+++ Tyuiu.LomakinVI.Sprint5.Task1.V29/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LomakinVI.Sprint5.Task1.V29.Lib;
+
+namespace Tyuiu.LomakinVI.Sprint5.Task1.V29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.Title = "Спринт #5 | Выполнил: Ломакин В. И. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант 29                                                              *");
+            Console.WriteLine("* Выполнил: Ломакин Владислав Игоревич | ИИПб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция. Произвести табулирование на диапозоне [-5; 5] с шагом 1.  *");
+            Console.WriteLine("* Результат сохранить в текстовый файл OutPutFileTask1.txt и вывести на   *");
+            Console.WriteLine("* консоль. Значения округлить до двух знаков после запятой.               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("F(x) = sin(x) / (x + 1.2) + cos(x) * 7x - 2");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
